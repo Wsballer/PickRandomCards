78e452a2c26db08813440cc1dbd62d84d8ce32e7
Tunji Oyewole <t.oyewole23@outlook.com>
2021-05-19 15:59:41 -0400

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                // this block is executed if line COULD be converted to an int
+                // value that's stored in a new variable called numberOfCards
+                string[] cards = CardPicker.PickSomeCards(numberOfCards);
+                foreach (string card in cards)
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                // this block is executed if line COULD NOT be converted to an int
+                Console.WriteLine("Not a valid number.");
+            }
+        }
+    }
+}
